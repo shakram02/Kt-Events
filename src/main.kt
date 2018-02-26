2253feb6b2299f42b62e0320cbf07f90fe497378
shakram02 <ahmedhamdyau@gmail.com>
2018-02-26 08:34:06 +0200

    add example

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
-    
+    val e = Event<Int>()
+
+    val handler: (Int) -> Unit = { arg -> println(arg) }
+
+    e += handler
+    e.invoke(4)
+
+    e.clear()
+    e.invoke(21312)
 }
